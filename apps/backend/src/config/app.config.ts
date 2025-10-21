5783744931de2bb6395eac22bf64cec482bf072c
Matias Demian Sayago <119746498+TheMishi99@users.noreply.github.com>
2025-10-21 06:04:26 -0300

    Change environment file from .env.production to .env

--- apps/backend/src/config/app.config.ts
+++ apps/backend/src/config/app.config.ts
@@ -1,4 +1,4 @@
-process.loadEnvFile(".env.production");
+process.loadEnvFile(".env");
 
 export const { JWT_SECRET, FRONT_URL, PORT } = process.env;
 export const isProduction = process.env.NODE_ENV === ".env.production";
